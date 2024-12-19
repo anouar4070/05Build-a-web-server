cc45d6b149d793976fc1477c23851636699b48b6
anouar garbaa <g.anouar@yahoo.com>
2024-12-19 21:22:48 +0100

    add errorHandler middleware

--- server.js
+++ server.js
@@ -3,12 +3,32 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
 app.use(logger);
 
-
+// Cross Origin Resource Sharing
+const whiteList = [
+  "https://www.yoursite.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    //if the domain (origin) is in the white list
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+      //null: Indicates that there is no error.
+      //true: Authorizes the request by indicating that the origin is valid.
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 //built-in middleware to handle urlencoded data (form data)
 //'content-type: application/x-www-form-urlencoded'
@@ -64,5 +84,7 @@ app.get("/chain(.html)?", [one, two, three]);
 app.get("/*", (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
