aa3cad28a2124d873d8c4b8c451d66e7defed614
anouar garbaa <g.anouar@yahoo.com>
2024-12-23 13:23:02 +0100

    enhancing auth,, logout, refresh Controllers to work with MongoDB

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,15 @@
-const usersDB = {
+/*const usersDB = {
   users: require("../model/users.json"),
   setUsers: function (data) {
     this.users = data;
   },
-};
+};*/
+const User = require("../model/User");
 const bcrypt = require("bcrypt");
-
 const jwt = require("jsonwebtoken");
 //require("dotenv").config();
-const fsPromises = require("fs").promises;
-const path = require("path");
+/*const fsPromises = require("fs").promises;
+const path = require("path");*/
 
 const handleLogin = async (req, res) => {
   const { user, pwd } = req.body;
@@ -17,7 +17,8 @@ const handleLogin = async (req, res) => {
     return res
       .status(400)
       .json({ message: "Username and password are required." });
-  const foundUser = usersDB.users.find((person) => person.username === user);
+  // const foundUser = usersDB.users.find((person) => person.username === user);
+  const foundUser = await User.findOne({ username: user }).exec();
   if (!foundUser) return res.sendStatus(401); //Unauthorized
   // evaluate password
   const match = await bcrypt.compare(pwd, foundUser.password);
@@ -25,10 +26,10 @@ const handleLogin = async (req, res) => {
     const roles = Object.values(foundUser.roles);
     // create JWTs
     const accessToken = jwt.sign(
-      { 
-        "UserInfo": {
-          "username": foundUser.username,
-          "roles": roles
+      {
+        UserInfo: {
+          username: foundUser.username,
+          roles: roles,
         },
       },
       process.env.ACCESS_TOKEN_SECRET,
@@ -40,7 +41,8 @@ const handleLogin = async (req, res) => {
       { expiresIn: "1d" }
     );
     // Saving refreshToken with current user
-    const otherUsers = usersDB.users.filter(
+    /* 
+   const otherUsers = usersDB.users.filter(
       (person) => person.username !== foundUser.username
     );
     const currentUser = { ...foundUser, refreshToken };
@@ -53,6 +55,17 @@ const handleLogin = async (req, res) => {
       httpOnly: true, //it's not available to JS
       sameSite: "None",
       secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });*/
+    
+    foundUser.refreshToken = refreshToken;
+    const result = await foundUser.save();
+    console.log(result);
+
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true, //it's not available to JS
+      sameSite: "None",
+      //secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     });
     res.json({ accessToken });
