c1fbcf329ba930ec7e72e3c55e47e5e43f86c440
anouar garbaa <g.anouar@yahoo.com>
2024-12-23 14:58:17 +0100

    add CRUD

--- controllers/authController.js
+++ controllers/authController.js
@@ -33,7 +33,7 @@ const handleLogin = async (req, res) => {
         },
       },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "30s" }
+      { expiresIn: "60s" }
     );
     const refreshToken = jwt.sign(
       { username: foundUser.username },
